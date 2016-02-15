9023941f93e774fc96aea1379a3e3a1505c2f013
Jean <Jean@PcdeJean>
2016-02-15 11:39:25 +0100

    Ajout des tests JUnit

--- InscriptionsSportives/src/inscriptions/Candidat.java
+++ InscriptionsSportives/src/inscriptions/Candidat.java
@@ -6,7 +6,7 @@ import java.util.Set;
 import java.util.TreeSet;
 
 /**
- * Candidat à un événement sportif, soit une personne physique, soit une équipe.
+ * Candidat Ã  un Ã©vÃ©nement sportif, soit une personne physique, soit une Ã©quipe.
  *
  */
 
@@ -19,7 +19,7 @@ public abstract class Candidat implements Comparable<Candidat>, Serializable
 	
 	Candidat(Inscriptions inscriptions, String nom)
 	{
-		this.inscriptions = inscriptions;	
+		this.inscriptions = inscriptions;
 		this.nom = nom;
 		competitions = new TreeSet<>();
 	}
@@ -45,7 +45,7 @@ public abstract class Candidat implements Comparable<Candidat>, Serializable
 	}
 
 	/**
-	 * Retourne toutes les compétitions auxquelles ce candidat est inscrit.s
+	 * Retourne toutes les compÃ©titions auxquelles ce candidat est inscrit.s
 	 * @return
 	 */
 
@@ -84,6 +84,6 @@ public abstract class Candidat implements Comparable<Candidat>, Serializable
 	@Override
 	public String toString()
 	{
-		return "\n" + getNom() + " -> inscrit à " + getCompetitions();
+		return "\n" + getNom() + " -> inscrit Ã  " + getCompetitions();
 	}
 }
