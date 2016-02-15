2b1f239135ea553622cf2f7ad58e35dc2263611f
Jean <Jean@PcdeJean>
2016-02-15 15:03:33 +0100

    presque finalisation test u competition

--- InscriptionsSportives/src/tests/CompetitionTest.java
+++ InscriptionsSportives/src/tests/CompetitionTest.java
@@ -7,7 +7,9 @@ import java.time.LocalDate;
 import org.junit.Test;
 
 import inscriptions.Competition;
+import inscriptions.Equipe;
 import inscriptions.Inscriptions;
+import inscriptions.Personne;
 
 public class CompetitionTest {
 
@@ -18,14 +20,16 @@ public class CompetitionTest {
 
 	@Test
 	public void testGetNom() {
-		fail("Not yet implemented");
+		String nom = "test de l'inscription";
+		Inscriptions inscriptions = Inscriptions.getInscriptions();
+		Competition testCompet = inscriptions.createCompetition(nom, null, false);
+		assertEquals(nom, testCompet.getNom());
 	}
 
 	@Test
 	public void testInscriptionsOuvertes() {
-		LocalDate dateCloture1, dateCloture2;
-		dateCloture1 = LocalDate.parse("2015-01-10");
-		dateCloture2 = LocalDate.parse("2018-04-20");
+		LocalDate dateCloture1 = LocalDate.parse("2015-01-10");
+		LocalDate dateCloture2 = LocalDate.parse("2018-04-20");
 		Inscriptions inscriptions = Inscriptions.getInscriptions();
 		Competition testCompetVrai = inscriptions.createCompetition("test", dateCloture1, false);
 		Competition testCompetFaux = inscriptions.createCompetition("test", dateCloture2, false);
@@ -35,37 +39,115 @@ public class CompetitionTest {
 
 	@Test
 	public void testGetDateCloture() {
-		fail("Not yet implemented");
+		LocalDate dateCloture1 = LocalDate.parse("2019-01-10");
+		Inscriptions inscriptions = Inscriptions.getInscriptions();
+		Competition testCompet = inscriptions.createCompetition("test", dateCloture1, false);
+		assertEquals(dateCloture1, testCompet.getDateCloture());
 	}
 
 	@Test
 	public void testEstEnEquipe() {
-		fail("Not yet implemented");
+		Inscriptions inscriptions = Inscriptions.getInscriptions();
+		Competition testCompetTrue = inscriptions.createCompetition("test", null, true);
+		Competition testCompetFalse = inscriptions.createCompetition("test", null, false);
+		assertEquals(true, testCompetTrue.estEnEquipe());
+		assertEquals(false, testCompetFalse.estEnEquipe());
 	}
 
 	@Test
 	public void testSetDateCloture() {
-		fail("Not yet implemented");
+		LocalDate dateCloture1 = LocalDate.parse("2017-01-10");
+		Inscriptions inscriptions = Inscriptions.getInscriptions();
+		Competition testCompet = inscriptions.createCompetition("test", dateCloture1, false);
+		LocalDate nouvelleDate = LocalDate.parse("2018-02-12");
+		testCompet.setDateCloture(nouvelleDate);
+		assertEquals(nouvelleDate, testCompet.getDateCloture());
 	}
 
 	@Test
 	public void testGetCandidats() {
-		fail("Not yet implemented");
+		Inscriptions inscriptions = Inscriptions.getInscriptions();
+		Competition testCompet = inscriptions.createCompetition("test", LocalDate.parse
+				("2018-02-10"), false);
+		Personne tony = inscriptions.createPersonne("Tony", null, null);
+		Personne robert = inscriptions.createPersonne("Robert", null, null);
+		Personne arthur = inscriptions.createPersonne("Arthur", null, null);
+		testCompet.add(tony);
+		testCompet.add(robert);
+		assertEquals(true, testCompet.getCandidats().contains(tony));
+		assertEquals(true, testCompet.getCandidats().contains(robert));
+		assertEquals(false, testCompet.getCandidats().contains(arthur));	
 	}
 
+//	TEST ADD PERSONNE
+	
 	@Test
-	public void testAddPersonne() {
-		fail("Not yet implemented");
+	public void testAddPersonneNormal() {
+		Inscriptions inscriptions = Inscriptions.getInscriptions();
+		Competition testCompet = inscriptions.createCompetition("test", LocalDate.parse
+				("2018-02-10"), false);
+		Personne tony = inscriptions.createPersonne("Tony", null, null);
+		testCompet.add(tony);
+		assertEquals(true, testCompet.getCandidats().contains(tony));
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void testAddPersonneEquipe() throws Exception{
+		Inscriptions inscriptions = Inscriptions.getInscriptions();
+		Competition testCompetEquipe = inscriptions.createCompetition("test", LocalDate.parse
+				("2018-02-10"), true);
+		Personne tony = inscriptions.createPersonne("Tony", null, null);
+		testCompetEquipe.add(tony);
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void testAddPersonneClos() throws Exception{
+		Inscriptions inscriptions = Inscriptions.getInscriptions();
+		Competition testCompetClose = inscriptions.createCompetition("test", LocalDate.parse
+				("2015-02-10"), false);
+		Personne tony = inscriptions.createPersonne("Tony", null, null);
+		testCompetClose.add(tony);
 	}
 
+	// TEST ADD EQUIPE
+	
 	@Test
-	public void testAddEquipe() {
-		fail("Not yet implemented");
+	public void testAddEquipeNormal() {
+		Inscriptions inscriptions = Inscriptions.getInscriptions();
+		Competition testCompet = inscriptions.createCompetition("test", LocalDate.parse
+				("2018-02-10"), true);
+		Equipe manouches = inscriptions.createEquipe("Les manouches");
+		testCompet.add(manouches);
+		assertEquals(true, testCompet.getCandidats().contains(manouches));
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void testAddEquipePersonne() throws Exception{
+		Inscriptions inscriptions = Inscriptions.getInscriptions();
+		Competition testCompetPersonne = inscriptions.createCompetition("test", LocalDate.parse
+				("2018-02-10"), false);
+		Equipe manouches = inscriptions.createEquipe("Les manouches");
+		testCompetPersonne.add(manouches);
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void testAddEquipeClos() throws Exception{
+		Inscriptions inscriptions = Inscriptions.getInscriptions();
+		Competition testCompetClose = inscriptions.createCompetition("test", LocalDate.parse
+				("2015-02-10"), true);
+		Equipe manouches = inscriptions.createEquipe("Les manouches");
+		testCompetClose.add(manouches);
 	}
 
 	@Test
 	public void testRemove() {
-		fail("Not yet implemented");
+		Inscriptions inscriptions = Inscriptions.getInscriptions();
+		Competition testCompet = inscriptions.createCompetition("test", LocalDate.parse
+				("2018-02-10"), false);
+		Personne tony = inscriptions.createPersonne("Tony", null, null);
+		testCompet.add(tony);
+		testCompet.remove(tony);
+		assertEquals(false, testCompet.getCandidats().contains(tony));
 	}
 
 	@Test
