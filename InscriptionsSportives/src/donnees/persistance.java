3dc47a1d6c76fc618c40c0221462dce523eff129
Jean <Jean@PcdeJean>
2016-03-17 11:22:54 +0100

    commit règlage problème conflit

--- InscriptionsSportives/src/donnees/persistance.java
+++ InscriptionsSportives/src/donnees/persistance.java
@@ -32,10 +32,6 @@ public class persistance {
 	String query;
 	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 	
-	
-	
-	
-    
     /**
      * Cree une connexion à la base de données
      */
