0b8bf904e40361b62e8200ab92dfef9c629046f8
thomas <thomasecalle@hotmail.fr>
2016-03-23 16:06:06 +0100

    test d'une fenêtre fxml

---
+++ InscriptionsSportives/src/interfaceGraphique/view/modificationEquipeController.java
@@ -0,0 +1,32 @@
+package interfaceGraphique.view;
+
+import inscriptions.Competition;
+import inscriptions.Equipe;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class modificationEquipeController {
+
+	
+	
+	@FXML
+    private TextField nomEquipe;
+	
+	
+	 @FXML
+	    private void handleValider() 
+	 {
+	        
+	 }
+	 
+	 @FXML
+	    private void handleAnnuler() 
+	 {
+	        
+	 }
+	
+	
+}
